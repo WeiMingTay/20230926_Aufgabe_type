d925a65f2e8a9a616889395f5ba59a8b8518ea4c
WeiMing <110397919+WeiMingTay@users.noreply.github.com>
2023-09-26 18:11:07 +0200

    Bonus - Gesamtdurchschnittsnote

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ type grades = 1 | 2 | 3 | 4 | 5 | 6 | "A" | "B" | "C" | "D" | "E" | "F" | undefi
 
 function printStudent(student: Student) {
     let name = student.firstName + " " + student.lastName + " (" + student.age + ")"
-    let noten = student.report.map(r => r.class + ": " + r.grade.map(note => ((note !== undefined) ? note.toString() : '*')).join(", ") + "\n").join("")
+    let noten = student.report.map(r => r.class + ": " + r.grade.map(note => ((note !== undefined) ? note : '*')).join(", ") + "\n").join("")
     console.log("%c" +
         name + "\n" +
         '='.repeat(name.length - 2) + "\n" +
@@ -27,22 +27,85 @@ function printStudentList(studentList: Student[]) {
         printStudent(student)
     }
 }
-function averageGrage(studentList: Student) {
-    for(let i = 0; i < studentList.report.length; i++) {
-        console.log(studentList.report[i].grade)
+
+//*
+// function averageGrade(studentList: Student[]) {
+//
+//
+//     for (let student of studentList) {
+//         const validGrades = student.report.filter(note => (note.grade !== undefined)).map(note => note.grade)
+//         console.log(validGrades)
+//         if (validGrades.length === 0) {
+//             return 0;
+//         }
+//         const sum = validGrades.reduce((acc, grade) => {
+//             if (typeof grade === "number") {
+//                 return acc + grade;
+//             } else if (grade === "A") {
+//                 return acc + 1;
+//             } else if (grade === "B") {
+//                 return acc + 2;
+//             } else if (grade === "C") {
+//                 return acc + 3;
+//             } else if (grade === "D") {
+//                 return acc + 4;
+//             } else if (grade === "E") {
+//                 return acc + 5;
+//             } else if (grade === "F") {
+//                 return acc + 6;
+//             }
+//             return acc;
+//         }, 0)
+//         return sum;
+//     }
+//
+//
+// }*/
+
+
+function averageGrade(studentList: Student[]): number {
+    let totalSum = 0;
+    let totalValidGrades = 0;
+
+    for (let student of studentList) {
+        const validGrades = student.report.flatMap(report => report.grade).filter(grade => grade !== undefined);
+        if (validGrades.length > 0) {
+            const sum = validGrades.reduce((acc, grade) => {
+                if (typeof grade === "number") {
+                    return acc + grade;
+                } else if (grade === "A") {
+                    return acc + 1;
+                } else if (grade === "B") {
+                    return acc + 2;
+                } else if (grade === "C") {
+                    return acc + 3;
+                } else if (grade === "D") {
+                    return acc + 4;
+                } else if (grade === "E") {
+                    return acc + 5;
+                } else if (grade === "F") {
+                    return acc + 6;
+                }
+                return acc;
+            }, 0);
+            totalSum += sum;
+            totalValidGrades += validGrades.length;
+        }
     }
 
+    if (totalValidGrades === 0) {
+        return 0; // Falls keine gültigen Noten vorhanden sind, wird der Durchschnitt als 0 angenommen.
+    }
 
+    return totalSum / totalValidGrades;
 }
-
-averageGrage(students)
 
 const students: Student[] = [
     {
         firstName: 'Ming',
         lastName: 'Tay',
         age: 18,
-        report: [{class: "Chemie", grade: ["B", 1, undefined]}, {class: "Kunst", grade: ["A", 1]}]
+        report: [{class: "Chemie", grade: ["B", 3, 2, undefined]}, {class: "Kunst", grade: ["A", 1]}]
     },
     {
         firstName: 'Helene',
@@ -55,7 +118,13 @@ const students: Student[] = [
         firstName: 'Summi',
         lastName: 'Pinsuwan',
         age: 7,
-        report: [{class: "Chemie", grade: ["B"]}]
+        report: [{class: "Chemie", grade: ["B"]}, {class: "Biologie", grade: ["B", 3, 2, undefined]}]
 
     }]
 printStudentList(students)
+console.log("Gesamtdurchschnittsnote: " + averageGrade([{
+    firstName: 'Ming',
+    lastName: 'Tay',
+    age: 18,
+    report: [{class: "Chemie", grade: ["B", 3, 2, undefined]}, {class: "Kunst", grade: ["A", 1]}]
+}]).toFixed(1))
